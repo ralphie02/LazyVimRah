bbefeb33a9fab5494f1f7455cf9baee03f4f81b4
Ralph Azucena <ralphie02@live.com>
2024-04-08 22:36:01 -0700

    add plugin extra test

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,10 +14,12 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- include copilot
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
+
+    -- include copilot
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.test.core" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
