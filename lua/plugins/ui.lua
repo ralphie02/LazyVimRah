4f7610acf0254d1b3e793016fde8f3f5de67190c
Ralph Azucena <ralphie02@live.com>
2024-04-12 11:54:13 -0700

    made lualine more sane (for me)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -52,15 +52,116 @@ return {
     end,
   },
   {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      -- lazyvim comment:
+      -- PERF: we don't need this lualine require madness 🤷
+      local lualine_require = require("lualine_require")
+      lualine_require.require = require
+
+      local icons = require("lazyvim.config").icons
+
+      vim.o.laststatus = vim.g.lualine_laststatus
+
+      local my_catppuccin_frappe = require('lualine.themes.catppuccin-frappe')
+      my_catppuccin_frappe.inactive.c = { fg = '#2c323c', bg = 'silver' }
+
+      return {
+        options = {
+          theme = my_catppuccin_frappe,
+          globalstatus = false,
+          disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+          -- remove separators to get more space; useful when branch and filepaths are long
+          component_separators = {},
+          section_separators = {},
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch" },
+
+          lualine_c = {
+            LazyVim.lualine.root_dir(),
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+              padding = { left = 1, right = 0 },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { 'filename', path = 1, padding = 0, buffers_color = { active = "lualine_a_inactive" } },
+          },
+          lualine_x = {
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = LazyVim.ui.fg("Statement"),
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = LazyVim.ui.fg("Constant"),
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = LazyVim.ui.fg("Debug"),
+            },
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = LazyVim.ui.fg("Special"),
+            },
+          },
+          lualine_y = {
+            {
+              "diff",
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_z = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+        },
+        inactive_sections = {
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { 'encoding', 'fileformat' },
+        },
+        extensions = { "nvim-tree", "lazy" },
+      }
+    end,
+  },
+  {
     "nvimdev/dashboard-nvim",
     opts = function()
       local logo = [[
         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗███████╗  █████╗ ██╗  ██╗          Z
-        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║██╔═══██╗██╔══██╗██║  ██║      Z    
-        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║████████║███████║███████║   z       
-        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║██╔══██╔╝██╔══██║██╔══██║ z         
-        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║██║  ╚██╗██║  ██║██║  ██║           
-        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝   ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝           
+        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║██╔═══██╗██╔══██╗██║  ██║      Z
+        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║████████║███████║███████║   z
+        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║██╔══██╔╝██╔══██║██╔══██║ z
+        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║██║  ╚██╗██║  ██║██║  ██║
+        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝   ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
       ]]
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
