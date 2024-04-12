dc51ca910d3b8dc75fd0e98816a377fc6e8a9ec5
Ralph Azucena <ralphie02@live.com>
2024-04-12 16:07:11 -0700

    good enuff for now

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,9 @@
 vim.keymap.del({ "n", "i", "v" }, "<A-j>")
 vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
+local toggle_rspec = [[:lcd `git rev-parse --show-toplevel` | if expand("%:r")[-5:-1] == "_spec" |
+  exe "e ".expand("%:h:s?spec?app?")."/".expand("%:t:s?_spec.rb?.rb?") | else |
+  exe "e ".expand("%:h:s?app?spec?")."/".expand("%:t:s?.rb?_spec.rb?") | endif
+]]
+vim.keymap.set('n', '<leader>tf', toggle_rspec, { silent = true, desc = "Toggle test (f)ile" })
 vim.keymap.set('v', 'y', '"+y') -- v-mode: yank to clipboard;
