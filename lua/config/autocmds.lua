d831d424695f9397427b41ec895abe394d8aded7
Ralph Azucena <ralphie02@live.com>
2024-04-15 00:12:39 -0700

    uncomment useless thing

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,8 +14,9 @@ vim.api.nvim_create_autocmd("User", {
     -- NOTE: Use vim.schedule when you need to perform an action asynchronously
     --  or when you want to defer execution until the event loop is ready.
     --  The function is run *before* any lua code above it.
-    vim.schedule(function()
-      -- vim.notify("I run before the vim.notify above")
-    end)
+
+    -- vim.schedule(function()
+    --   vim.notify("I run before the vim.notify above")
+    -- end)
   end,
 })
