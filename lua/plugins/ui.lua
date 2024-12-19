0b185316814b4ba8530500dfc55588aa022fda00
Ralph Azucena <ralphie02@live.com>
2024-12-19 13:43:28 -0800

    fix

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -75,7 +75,7 @@ return {
           theme = my_catppuccin_frappe,
           -- MY EDIT: END --
 
-          globalstatus = vim.o.laststatus == 3,
+          globalstatus = false,
           disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
 
           -- MY EDIT: START --
@@ -174,6 +174,28 @@ return {
         },
         extensions = { "nvim-tree", "lazy" },
       }
+
+      -- do not add trouble symbols if aerial is enabled
+      -- And allow it to be overriden for some buffer types (see autocmds)
+      if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
+        local trouble = require("trouble")
+        local symbols = trouble.statusline({
+          mode = "symbols",
+          groups = {},
+          title = false,
+          filter = { range = true },
+          format = "{kind_icon}{symbol.name:Normal}",
+          hl_group = "lualine_c_normal",
+        })
+        table.insert(opts.sections.lualine_c, {
+          symbols and symbols.get,
+          cond = function()
+            return vim.b.trouble_lualine ~= false and symbols.has()
+          end,
+        })
+      end
+
+      return opts
     end,
   },
   {
