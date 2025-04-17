867d721fadc7dac13289fc086711b9cbada63161
Ralph Azucena <ralphie02@live.com>
2025-04-17 15:53:17 -0700

    refactor

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,18 +7,24 @@ vim.keymap.del({ "n", "i", "v" }, "<A-j>")
 vim.keymap.del({ "n", "i", "v" }, "<A-k>")
 
 vim.keymap.set('v', 'y', '"+y') -- v-mode: yank to clipboard;
+local function toggle_rb_spec()
+  -- rails
+  if vim.fn.filereadable("config/application.rb") == 1 then
+    if vim.fn.expand("%:t"):match("_spec%.rb$") then
+      vim.cmd("edit " .. vim.fn.expand("%:h"):gsub("spec", "app") .. "/" .. vim.fn.expand("%:t"):gsub("_spec%.rb$", ".rb"))
+    else
+      vim.cmd("edit " .. vim.fn.expand("%:h"):gsub("app", "spec") .. "/" .. vim.fn.expand("%:t"):gsub("%.rb$", "_spec.rb"))
+    end
+  end
+
+end
 local function toggle_rspec()
   vim.cmd("lcd " .. vim.fn.system("git rev-parse --show-toplevel"):gsub("\n", ""))
-  -- Guard clause: Exit if the file is not a Ruby file or if not in a Rails project
-  if vim.fn.expand("%:e") ~= "rb" or not vim.fn.isdirectory("app") or vim.fn.filereadable("Gemfile") == 0 then
-    vim.notify("Not a Ruby file or Rails project", vim.log.levels.WARN)
-    return
-  end
-  -- Check if the current file is a spec file
-  if vim.fn.expand("%:t"):match("_spec%.rb$") then
-    vim.cmd("edit " .. vim.fn.expand("%:h"):gsub("spec", "app") .. "/" .. vim.fn.expand("%:t"):gsub("_spec%.rb$", ".rb"))
+
+  if vim.bo.filetype == "ruby" and vim.fn.filereadable("Gemfile") == 1 then
+    toggle_rb_spec()
   else
-    vim.cmd("edit " .. vim.fn.expand("%:h"):gsub("app", "spec") .. "/" .. vim.fn.expand("%:t"):gsub("%.rb$", "_spec.rb"))
+    vim.notify("Unsupported file type and/or project", vim.log.levels.WARN)
   end
 end
 vim.keymap.set('n', '<leader>ti', toggle_rspec, { silent = true, desc = "Toggle Test F(i)le" })
