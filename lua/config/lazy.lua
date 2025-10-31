fbb1127f66636647b73e97651a6ff53ab73fa6e7
Ralph A <ralph.azucena@datadocks.com>
2025-10-30 22:18:23 -0700

    Add copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.ai.copilot" } ,
     { import = "lazyvim.plugins.extras.ai.avante" } , -- cursor-like features
 
     -- import/override with your plugins
