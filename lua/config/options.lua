655619acdb96e98ded1fa8a7114476445fad5def
Ralph Azucena <ralphie02@live.com>
2026-05-16 16:31:45 -0700

    no wrap tmux nav

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,7 @@ g.lazyvim_ruby_formatter = "rubocop"
 -- g.loaded_ruby_provider = 0
 -- g.ruby_path = vim.fn.system('rbenv prefix') -- github.com/vim-ruby/vim-ruby/issues/248
 g.lazyvim_rust_diagnostics = "rust-analyzer"
+g.tmux_navigator_no_wrap = 1
 
 -- "set regexpengine" == "opt.regexpengine" in case I forget again
 -- opt.regexpengine = 1 -- sourcing <nvim7-install..>/syntax/ruby.vim from ~18ms to ~9ms
